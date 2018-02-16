8b40a6f96c47844458fae1058b2c2f0796165d73
Hechen <381548325@qq.com>
2018-02-16 17:36:53 +0800

    第20课

---
+++ app.js
@@ -0,0 +1,55 @@
+const mysql = require('mysql');
+const express = require('express');
+const static = require('express-static');
+const bodyParser = require('body-parser');
+const multer = require('multer');  //处理上传的文件
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+const consolidate = require('consolidate');
+const ejs = require('ejs');
+const opn = require('opn');
+
+const db = mysql.createPool({host: 'localhost', user: 'root', password: '123456', database: 'blog'});
+
+const app = express();
+
+//解析cookie
+app.use(cookieParser('1g321jkj21a87sd80a8sd7089asd'));
+
+//使用session
+let arr = [];
+for(let i = 0; i < 100000; i++) {
+	arr.push('keys_' + Math.random());
+}
+app.use(cookieSession({name: 'demo_id', keys: arr, maxAge: 20*60*60*1000}));
+
+//输出什么东西
+
+app.set('view engine', 'html');
+
+//模板引擎放在哪
+
+app.set('views', './template');
+
+//哪种模板引擎
+
+app.engine('html', consolidate.ejs);
+
+//接受用户请求
+
+app.get('/', (req, res)=>{
+	//查询banner的东西
+	db.query("SELECT * FROM banner_table", (err, data)=> {
+		if(err) {
+			res.status(500).send('err').end();
+		}else{
+			res.render('index.ejs', {banners: data});
+		}
+	});
+});
+
+app.use(static('./www'));
+
+app.listen(3001);
+
+opn('http://localhost:3001');
